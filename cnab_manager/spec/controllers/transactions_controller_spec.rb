0d5ef5aec5f945dd571a9314299f57dd49388cee
Julio Lara <juscezari@gmail.com>
2020-10-25 17:39:11 -0300

    Added tests for uploaded files

--- cnab_manager/spec/controllers/transactions_controller_spec.rb
+++ cnab_manager/spec/controllers/transactions_controller_spec.rb
@@ -22,5 +22,37 @@ RSpec.describe TransactionsController, type: :controller do
       post :upload
       expect(response).to redirect_to transactions_url
     end
+
+    context 'when uploading new file with CNAB records' do
+      
+      let(:number_of_records_in_mocked_file) { 21 }
+      let(:fixture_file_path) { Rails.root.join('spec', 'fixtures', 'files', 'CNAB.txt') }
+      let(:mocked_file) { Rack::Test::UploadedFile.new(fixture_file_path, 'text/plain', false) }
+
+      before :example do
+        Transaction.destroy_all
+      end
+
+      it 'should save all the CNAB records present in the file' do
+        params = { "transactions" => { "transactions_file" => mocked_file } }
+        expect{
+          post :upload, params: params
+        }.to change(Transaction, :count).by(number_of_records_in_mocked_file)        
+      end
+
+      it 'should set a message in flash[:success] showing how many transactions were saved' do
+        params = { "transactions" => { "transactions_file" => mocked_file } }
+        post :upload, params: params
+        expect(flash[:success]).to match("#{number_of_records_in_mocked_file} transações adicionadas ao sistema")
+      end
+
+      it 'should set a message in flash[:notice] if no file is provided' do
+        params = { "transactions" => { "transactions_file" => nil } }
+        post :upload, params: params
+        expect(flash[:notice]).to match('Nenhum arquivo enviado')
+      end
+
+    end
+    
   end
 end
